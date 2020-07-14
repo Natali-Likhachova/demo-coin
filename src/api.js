0d4de6d9e89e11d9a46e75eb30f370e0e7686caa
Natali <nataliya.likhachova@gmail.com>
2020-07-14 22:19:29 +0300

    added api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const  HOST = 'http://coins.demo.javascript.ninja';
 
-// eslint-disable-next-line import/prefer-default-export
+// eslint-disable-next-line
 export function getTicker(pair) {
     return fetch(`${HOST}/ticker/${pair}`)
         .then(r => r.json())

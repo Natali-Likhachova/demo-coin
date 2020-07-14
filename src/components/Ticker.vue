5139505b57d6458b7884d173cb7ea52c07e92667
Natali <nataliya.likhachova@gmail.com>
2020-07-14 21:54:14 +0300

    data is received from the server

--- src/components/Ticker.vue
+++ src/components/Ticker.vue
@@ -1,9 +1,28 @@
 <template>
-    <div>
-        Hello
+    <div class="root">
+        <h1>{{ pair }}</h1>
+        <h2>{{ price }}</h2>
+        <i class="fas fa-cog fa-spin"></i>
     </div>
 </template>
 
+<style scoped>
+    .root{
+        border: solid black 3px;
+        border-radius: 5px;
+        padding: 10px;
+        margin: 10px;
+        text-align: center;
+    }
+</style>
+
 <script>
+    export default {
+        data: () => ({
+            pair: 'BTC_USD',
+            price: '',
+            isLoading: true,
+        }),
+    };
 </script>
 

224fb6ccfa9bfe24226ca11b0ee42ec64b0c0f77
Natali <nataliya.likhachova@gmail.com>
2020-07-18 11:23:14 +0300

    added List

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <List />
     <Ticker pair="btc_usd" />
     <Ticker pair="eth_usd" />
   </div>
@@ -7,10 +8,13 @@
 
 <script>
 import Ticker from './components/Ticker.vue'
+import List from './components/List.vue'
+import List from "./components/List";
 
 export default {
   name: 'App',
   components: {
+    List,
     Ticker
   }
 }
---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="root">
+        <ul>
+            <li :key="key" v-for="(value, key) in pairs">{{ key }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .root{
+        display: inline-block;
+        border: solid black 3px;
+        border-radius: 5px;
+        padding: 10px;
+        margin: 10px;
+        text-align: center;
+    }
+</style>
+
+<script>
+    import pairsData from '../pairs.json';
+
+    export default {
+        data: () => ({
+            pairs: pairsData.pairs,
+        }),
+    };
+
+</script>
+
